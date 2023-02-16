342972a86cf1a6b403a5564e33a2ac56b3d7b3ef
Messai <messai@github.com>
2023-02-16 20:36:49 +0300

    Started the repo and added some files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using Bubble sort
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int tmp;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		for (i = 0; i < size - j - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+			}
+			else
+			{
+				continue;
+			}
+			print_array(array, size);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Sorting Algorithms
+
+Project done on different types of sorting algorithms.
+
+
