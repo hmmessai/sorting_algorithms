8a0e029a2486585d1712d2f41d7852172b8d28d9
Messai <messai@github.com>
2023-02-18 10:30:32 +0300

    Added 2

--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_till_end(listint_t **list, listint_t **element, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif
